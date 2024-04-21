4796957fd414afc3963e5928ffee103dc217d38c
Amruta Naik <naik.amr@northeastern.edu>
2024-04-21 16:48:55 -0400

    added file for backend code

---
+++ Backend/CarPricing.java
@@ -0,0 +1,287 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author Amruta Naik
+ */
+public class CarPricing {
+String companyname;
+int base_premium;
+int Y2022_21;
+int Y2020_19;
+int Y2018_15;
+int Y2014_10;
+int Y2010_and_Below;
+int BMW;
+int Audi;
+int Toyota;
+int Tesla;
+int X1;
+int M3;
+int X3;
+int X5;
+int A3;
+int A4;
+int A6;
+int Q3;
+int Corolla;
+int Prius;
+int HighlanderHybrid;
+
+    public void setY2022_21(int Y2022_21) {
+        this.Y2022_21 = Y2022_21;
+    }
+
+    public void setY2020_19(int Y2020_19) {
+        this.Y2020_19 = Y2020_19;
+    }
+
+    public void setY2018_15(int Y2018_15) {
+        this.Y2018_15 = Y2018_15;
+    }
+
+    public void setY2014_10(int Y2014_10) {
+        this.Y2014_10 = Y2014_10;
+    }
+
+    public void setY2010_and_Below(int Y2010_and_Below) {
+        this.Y2010_and_Below = Y2010_and_Below;
+    }
+
+    public void setBMW(int BMW) {
+        this.BMW = BMW;
+    }
+
+    public void setAudi(int Audi) {
+        this.Audi = Audi;
+    }
+
+    public void setToyota(int Toyota) {
+        this.Toyota = Toyota;
+    }
+
+    public void setTesla(int Tesla) {
+        this.Tesla = Tesla;
+    }
+
+    public void setX1(int X1) {
+        this.X1 = X1;
+    }
+
+    public void setM3(int M3) {
+        this.M3 = M3;
+    }
+
+    public void setX3(int X3) {
+        this.X3 = X3;
+    }
+
+    public void setX5(int X5) {
+        this.X5 = X5;
+    }
+
+    public void setA3(int A3) {
+        this.A3 = A3;
+    }
+
+    public void setA4(int A4) {
+        this.A4 = A4;
+    }
+
+    public void setA6(int A6) {
+        this.A6 = A6;
+    }
+
+    public void setQ3(int Q3) {
+        this.Q3 = Q3;
+    }
+
+    public void setCorolla(int Corolla) {
+        this.Corolla = Corolla;
+    }
+
+    public void setPrius(int Prius) {
+        this.Prius = Prius;
+    }
+
+    public void setHighlanderHybrid(int HighlanderHybrid) {
+        this.HighlanderHybrid = HighlanderHybrid;
+    }
+
+    public void setY4_Runner(int Y4_Runner) {
+        this.Y4_Runner = Y4_Runner;
+    }
+
+    public void setY3(int Y3) {
+        this.Y3 = Y3;
+    }
+
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+
+    public void setS(int S) {
+        this.S = S;
+    }
+
+    public void setX(int X) {
+        this.X = X;
+    }
+
+    public int getY2022_21() {
+        return Y2022_21;
+    }
+
+    public int getY2020_19() {
+        return Y2020_19;
+    }
+
+    public int getY2018_15() {
+        return Y2018_15;
+    }
+
+    public int getY2014_10() {
+        return Y2014_10;
+    }
+
+    public int getY2010_and_Below() {
+        return Y2010_and_Below;
+    }
+
+    public int getBMW() {
+        return BMW;
+    }
+
+    public int getAudi() {
+        return Audi;
+    }
+
+    public int getToyota() {
+        return Toyota;
+    }
+
+    public int getTesla() {
+        return Tesla;
+    }
+
+    public int getX1() {
+        return X1;
+    }
+
+    public int getM3() {
+        return M3;
+    }
+
+    public int getX3() {
+        return X3;
+    }
+
+    public int getX5() {
+        return X5;
+    }
+
+    public int getA3() {
+        return A3;
+    }
+
+    public int getA4() {
+        return A4;
+    }
+
+    public int getA6() {
+        return A6;
+    }
+
+    public int getQ3() {
+        return Q3;
+    }
+
+    public int getCorolla() {
+        return Corolla;
+    }
+
+    public int getPrius() {
+        return Prius;
+    }
+
+    public int getHighlanderHybrid() {
+        return HighlanderHybrid;
+    }
+
+    public int getY4_Runner() {
+        return Y4_Runner;
+    }
+
+    public int getY3() {
+        return Y3;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public int getS() {
+        return S;
+    }
+
+    public int getX() {
+        return X;
+    }
+int Y4_Runner;
+int Y3;
+
+    public CarPricing(String companyname,int base_premium,int Y2022_21, int Y2020_19, int Y2018_15, int Y2014_10, int Y2010_and_Below, int BMW, int Audi, int Toyota, int Tesla, int X1, int M3, int X3, int X5, int A3, int A4, int A6, int Q3, int Corolla, int Prius, int HighlanderHybrid, int Y4_Runner, int Y3, int Y, int S, int X) {
+        this.companyname = companyname;
+        this.base_premium = base_premium;
+        this.Y2022_21 = Y2022_21;
+        this.Y2020_19 = Y2020_19;
+        this.Y2018_15 = Y2018_15;
+        this.Y2014_10 = Y2014_10;
+        this.Y2010_and_Below = Y2010_and_Below;
+        this.BMW = BMW;
+        this.Audi = Audi;
+        this.Toyota = Toyota;
+        this.Tesla = Tesla;
+        this.X1 = X1;
+        this.M3 = M3;
+        this.X3 = X3;
+        this.X5 = X5;
+        this.A3 = A3;
+        this.A4 = A4;
+        this.A6 = A6;
+        this.Q3 = Q3;
+        this.Y3 = Y3;
+        this.Y = Y;
+        this.S = S;
+        this.X = X;
+        this.Corolla = Corolla;
+        this.Prius = Prius;
+        this.HighlanderHybrid = HighlanderHybrid;
+        this.Y4_Runner = Y4_Runner;
+        
+    }
+
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+
+    public void setBase_premium(int base_premium) {
+        this.base_premium = base_premium;
+    }
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public int getBase_premium() {
+        return base_premium;
+    }
+int Y;
+int S;
+int X;
+
+}
